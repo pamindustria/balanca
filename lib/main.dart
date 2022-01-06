30675ec26853477304f03d854e6147f69d08c70d
adriavieira314 <adriavieira314@gmail.com>
2022-01-06 19:40:58 -0400

    ajustes

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.blue,
       ),
       initialRoute: '/',
       routes: {
--- lib/pages/tela_principal_page.dart
+++ lib/pages/tela_principal_page.dart
@@ -33,11 +33,11 @@ class _TelaPrincipalPageState extends State<TelaPrincipalPage> {
             'BALANÇA - EPS',
             style: TextStyle(fontSize: 40.0),
           ),
-          toolbarHeight: 80,
+          toolbarHeight: 70,
           centerTitle: true,
         ),
         body: Padding(
-          padding: const EdgeInsets.only(left: 18.0, right: 18.0),
+          padding: const EdgeInsets.only(left: 18.0, right: 18.0, top: 8.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
@@ -81,7 +81,7 @@ class _TelaPrincipalPageState extends State<TelaPrincipalPage> {
                       icon: null,
                       elevation: 16,
                       style: const TextStyle(
-                        fontSize: 30.0,
+                        fontSize: 39.0,
                         color: Colors.black,
                       ),
                       alignment: Alignment.center,
@@ -99,49 +99,99 @@ class _TelaPrincipalPageState extends State<TelaPrincipalPage> {
                       }).toList(),
                     ),
                   ),
-                  const PesoWidget(),
+                  const CustomPesoField(),
                 ],
               ),
               // primeira segunda - botao status serial e salvar
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Container(
-                    height: 50,
-                    width: 180,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.white,
-                      boxShadow: const [
-                        BoxShadow(color: Colors.green, spreadRadius: 2),
-                      ],
-                    ),
+                  SizedBox(
+                    width: 400,
                     child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Transform.scale(
-                          scale: 1.5,
-                          child: Checkbox(
-                            checkColor: Colors.white,
-                            value: isChecked,
-                            onChanged: (bool? value) {
-                              setState(() {
-                                isChecked = value!;
-                              });
-                            },
+                        Container(
+                          height: 50,
+                          width: 180,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white,
+                            boxShadow: const [
+                              BoxShadow(color: Colors.grey, spreadRadius: 1),
+                            ],
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Transform.scale(
+                                scale: 1.5,
+                                child: Checkbox(
+                                  checkColor: Colors.white,
+                                  value: isChecked,
+                                  onChanged: (bool? value) {
+                                    setState(() {
+                                      isChecked = value!;
+                                    });
+                                  },
+                                ),
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.only(left: 12.0),
+                                child: Text(
+                                  'Status Serial',
+                                  style: TextStyle(
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                        const Text(
-                          'Status Serial',
-                          style: TextStyle(
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.w500,
+                        Container(
+                          height: 50,
+                          width: 180,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white,
+                            boxShadow: const [
+                              BoxShadow(color: Colors.grey, spreadRadius: 1),
+                            ],
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Transform.scale(
+                                scale: 1.5,
+                                child: Checkbox(
+                                  checkColor: Colors.white,
+                                  value: isChecked,
+                                  onChanged: (bool? value) {
+                                    setState(() {
+                                      isChecked = value!;
+                                    });
+                                  },
+                                ),
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.only(left: 13.0),
+                                child: Text(
+                                  'Status SQL',
+                                  style: TextStyle(
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       ],
                     ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.only(right: 10.0),
+                    padding: const EdgeInsets.only(right: 0.0),
                     child: ElevatedButton(
                       onPressed: () {},
                       child: const Text(
@@ -151,9 +201,10 @@ class _TelaPrincipalPageState extends State<TelaPrincipalPage> {
                         ),
                       ),
                       style: ElevatedButton.styleFrom(
+                        primary: Colors.green,
                         padding: const EdgeInsets.symmetric(vertical: 15.0),
                         elevation: 5,
-                        minimumSize: const Size(180.0, 0),
+                        minimumSize: const Size(255.0, 0),
                       ),
                     ),
                   )
@@ -161,61 +212,17 @@ class _TelaPrincipalPageState extends State<TelaPrincipalPage> {
               ),
               // terceira linha - grafico
               Container(
-                height: 500,
+                height: 400,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10),
                   color: Colors.white,
                   boxShadow: const [
-                    BoxShadow(color: Colors.green, spreadRadius: 2),
+                    BoxShadow(color: Colors.grey, spreadRadius: 1),
                   ],
                 ),
               )
             ],
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class PesoWidget extends StatelessWidget {
-  const PesoWidget({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 25.0),
-      child: Container(
-        height: 80,
-        width: 200,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Colors.white,
-          boxShadow: const [
-            BoxShadow(color: Colors.green, spreadRadius: 2),
-          ],
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: const [
-            Text(
-              'Peso',
-              style: TextStyle(
-                fontSize: 25.0,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            Text(
-              '0.000',
-              style: TextStyle(
-                fontSize: 35.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.red,
-              ),
-            ),
-          ],
         ),
       ),
     );
@@ -240,7 +247,7 @@ class CustomTextFormField extends StatelessWidget {
         child: TextFormField(
           autofocus: false,
           controller: controller,
-          style: const TextStyle(fontSize: 24.0),
+          style: const TextStyle(fontSize: 30.0),
           decoration: InputDecoration(
             // cor da borda
             enabledBorder: OutlineInputBorder(
@@ -254,6 +261,50 @@ class CustomTextFormField extends StatelessWidget {
               borderRadius: BorderRadius.circular(10),
             ),
             labelText: label,
+            labelStyle: const TextStyle(fontSize: 30.0),
+            floatingLabelStyle: const TextStyle(fontSize: 30.0),
+            fillColor: Colors.white,
+            filled: true,
+            floatingLabelBehavior: FloatingLabelBehavior.always,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomPesoField extends StatelessWidget {
+  const CustomPesoField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 18.0),
+        child: TextFormField(
+          autofocus: false,
+          initialValue: '0.000',
+          readOnly: true,
+          style: const TextStyle(
+            fontSize: 60.0,
+            color: Colors.red,
+            fontWeight: FontWeight.bold,
+          ),
+          decoration: InputDecoration(
+            contentPadding:
+                const EdgeInsets.only(left: 18.0, top: 0.0, bottom: 0.0),
+            // cor da borda
+            enabledBorder: OutlineInputBorder(
+              borderSide: const BorderSide(
+                color: Colors.grey,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            // border radius
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            labelText: 'Peso',
             labelStyle: const TextStyle(fontSize: 30.0),
             floatingLabelStyle: const TextStyle(fontSize: 30.0),
             fillColor: Colors.white,
